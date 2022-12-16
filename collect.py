be76a765e985f38b83816895e413449ae3fd64a8
Gabor Szabo <gabor@szabgab.com>
2022-12-16 14:30:47 +0200

    collect using the API_KEY

--- collect.py
+++ collect.py
@@ -5,27 +5,26 @@ import argparse
 import time
 import pathlib
 
-# TODO: use API key?
-# TODO: Maybe compare the github_username that arrived in the JSON to the same field supplied by the user when configured the job
-
-def collect(username, host, limit):
-    print(f"collect({username}, {host}, {limit})")
+def collect(host, limit):
+    print(f"collect({host}, {limit})")
 
     data = pathlib.Path.cwd().joinpath('data')
     if not data.exists():
         data.mkdir()
     print(f"Data dir: {data}")
 
-    per_page = 100
+    per_page = 30
 
-    api_key = os.environ.get('DEV_TO_API')
+    api_key = os.environ.get('FOREM_API_KEY')
+    if not api_key:
+        exit("FOREM_API_KEY missing")
 
     page = 0
     articles = {}
     statistics = {}
     while True:
         page += 1
-        url = f'https://{host}/api/articles?username={username}&page={page}&per_page={per_page}'
+        url = f'https://{host}/api/articles/me?page={page}&per_page={per_page}'
         # print(url)
 
         headers = {'Accept': 'application/vnd.forem.api-v1+json'}
@@ -43,6 +42,7 @@ def collect(username, host, limit):
             break
         for article in res.json():
             # print(article)
+            del article['body_markdown']
             articles[ article['id'] ] = article
             statistics[ article['id'] ] = {
                 'comments_count': article['comments_count'],
@@ -88,7 +88,7 @@ def get_args():
 
     parser = argparse.ArgumentParser(parents=[main_parser])
     if main_args.collect:
-        parser.add_argument('--username',  help='The username on the Forem site', required=main_args.collect)
+        #parser.add_argument('--username',  help='The username on the Forem site', required=main_args.collect)
         parser.add_argument('--host',      help='The hostname of the Forem site', required=main_args.collect)
         parser.add_argument('--limit',     help='Max number of pages to fetch', type=int)
 
@@ -99,13 +99,11 @@ def get_args():
 def main():
     args = get_args()
 
-    # github_username = os.environ.get('GITHUB_REPOSITORY_OWNER')
-
     if args.collect:
         hosts = ('dev.to', 'community.codenewbie.org')
         if args.host not in hosts:
             exit('Invalid host')
-        collect(args.username, args.host, args.limit)
+        collect(args.host, args.limit)
 
     if args.commit:
         commit()
